31ee5e5945447f049fdf62ea579efdae95bb6aa5
MiriamCall <bow18005@byui.edu>
2025-04-05 16:50:07 -0600

    Implement contact management features: add contact model methods, update controller, and enhance contact form view

--- src/controllers/contactController.js
+++ src/controllers/contactController.js
@@ -1,9 +1,12 @@
 // contactController.js
+import { getAllContacts, addNewContact } from "../models/contactModel.js";
 
-export const renderContactPage = (req, res) => {
+const renderContactPage = async (req, res) => {
   res.render("contact", {
     title: "Contact",
     errors: {},
     user: req.session.userId ? { id: req.session.userId } : null,
   });
 };
+
+export { renderContactPage };
--- src/models/contactModel.js
+++ src/models/contactModel.js
@@ -1,1 +1,36 @@
 import dbClient from "./index.js";
+
+const getContactById = async (id) => {
+  const query = "SELECT * FROM contacts WHERE id = $1";
+  const values = [id];
+  return await dbClient.query(query, values);
+};
+
+const getAllContacts = async () => {
+  const query = "SELECT * FROM contacts ORDER BY created_at DESC";
+  return await dbClient.query(query);
+};
+
+const addNewContact = async (contactData) => {
+  const query = `
+        INSERT INTO contacts (first_name, last_name, email, message)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *
+    `;
+  const values = [
+    contactData.first_name,
+    contactData.last_name,
+    contactData.email,
+    contactData.message,
+  ];
+
+  return await dbClient.query(query, values);
+};
+
+const deleteContact = async (id) => {
+  const query = "DELETE FROM contacts WHERE id = $1 RETURNING *";
+  const values = [id];
+  return await dbClient.query(query, values);
+};
+
+export { getContactById, getAllContacts, addNewContact, deleteContact };
